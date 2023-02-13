f193514e27f993377c17879269aa9ebd275e8ea8
Julia-slvbrg <julia.slvb@gmail.com>
2023-02-13 00:24:42 -0300

    estrutura de arquivos

---
+++ projeto-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <h1>Loja de itens para viagens espaciais</h1>
+      <p>em breve....</p>
+    </div>
+  )
+}
+
+export default App
